993d9efc173ab67e6a2b910981bafe9d09ab2320
Eh-haque <ehtisamul@gmail.com>
2026-07-30 22:55:46 +0600

    feat: home page added

--- donors/views.py
+++ donors/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import BloodRequest
+
+def home_view(request):
+    # Fetch recent urgent blood requests
+    recent_requests = BloodRequest.objects.filter(is_fulfilled=False).order_by('-created_at')[:5]
+    
+    context = {
+        'recent_requests': recent_requests,
+    }
+    return render(request, 'donors/home.html', context)
